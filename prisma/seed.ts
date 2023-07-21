f261b219bb74d31539396a36f2b30e849b0ee9b1
Ben Flanders <Benrflanders@gmail.com>
2023-07-20 20:16:28 -0400

    Use mock data when seeding dev database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,56 +1,14 @@
+import { mockTriviaEvents } from "@/mocks/events";
+import { mockTriviaVenues } from "@/mocks/venues";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const restraunt_1 = await prisma.restraunt.create({
-    data: {
-      name: "North River Tavern",
-      address: "8879 Roswell Rd",
-      city: "Sandy Springs",
-      state: "GA",
-      zipCode: 30350,
-      url: "https://northrivertavern.com/",
-      triviaEvents: {
-        create: [
-          {
-            name: "Team Trivia",
-            starts_at: new Date("2023-03-22T07:30"),
-            id: 1,
-          },
-          {
-            name: "Team Trivia",
-            starts_at: new Date("2023-03-23T08:00"),
-            id: 2,
-          },
-        ],
-      },
-    },
-  });
+  // create the venues
+  await prisma.triviaVenue.createMany({ data: mockTriviaVenues });
 
-  const restraunt_2 = await prisma.restraunt.create({
-    data: {
-      name: "Eventide Brewing",
-      address: "1015 Grant St Se",
-      city: "Atlanta",
-      state: "GA",
-      zipCode: 30315,
-      url: "https://eventidebrewing.com/",
-      triviaEvents: {
-        create: [
-          {
-            name: "Dirty South Trivia",
-            starts_at: new Date("2023-03-23T07:00"),
-            price: 0,
-          },
-          {
-            name: "Dirty South Trivia",
-            starts_at: new Date("2023-03-30T07:00"),
-            price: 0,
-          },
-        ],
-      },
-    },
-  });
+  // add events to all the venues
+  await prisma.triviaEvent.createMany({ data: mockTriviaEvents });
 }
 
 main()
